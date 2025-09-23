ea2e344a25ad47717824db054f65eefda21ef405
Mosab <mosabsalman999@gmail.com>
2025-09-23 19:34:37 +0300

    solving tailwind config problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
+
+import React from 'react';
+
 export default function Home() {
-  return (
-    <>
-      <div className="flex min-w-full justify-center  min-h-screen items-center ">
-        {" "}
-        <div className="text-6xl justify-center flex min-h-full p-3 rounded-3xl text-black font-mono bg-red-500">
-          hello{" "}
-        </div>
-      </div>
-      ;
-    </>
-  );
+  return <div className='flex bg-red-300 text-cyan-700 text-4xl '>home</div>;
 }
