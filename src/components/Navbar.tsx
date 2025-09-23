9095d3ea0a35301ab8cf4bd8d98ea969fc5cd0af
Mosab <mosabsalman999@gmail.com>
2025-09-23 19:50:12 +0300

    configuring  the globals-v4.css file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,28 +10,29 @@ const Navbar = () => {
       style={{ height: `${NAVBAR_HEIGHT}px` }}
     >
       <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
-        <div className="flex items-center gap-4 md:gap-6  cursor-pointer">
+        <div className="flex items-center gap-4 md:gap-6">
           <Link
             href="/"
             className="cursor-pointer hover:!text-primary-300"
             scroll={false}
-          />
-          <div className="flex items-center gap-3 ">
-            <Image
-              src="/logo.svg"
-              alt="Aqar"
-              width={24}
-              height={24}
-              className="w-6 h-6"
-            />
+          >
+            <div className="flex items-center gap-3 ">
+              <Image
+                src="/logo.svg"
+                alt="Rentiful Logo"
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
 
-            <div className="text-xl font-bold">
-              RENT
-              <span className="text-secondary-500 font-light hover:!text-primary-300  ">
-                IFUL
-              </span>
+              <div className="text-xl font-bold">
+                RENT
+                <span className="text-secondary font-light hover:!text-primary-300  ">
+                  IFUL
+                </span>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
