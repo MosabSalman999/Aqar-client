e49c616613bbfd23383689e491a836502bc2f4d8
Mosab <mosabsalman999@gmail.com>
2025-12-23 20:16:02 +0300

    chore: update ESLint configuration to disable specific TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,16 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules : {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unnecessary-type-assertion": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
